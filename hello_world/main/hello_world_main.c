c46336e91b483ae992263ca8c1b5c8bfae26c1ec
DESKTOP-EBIGPDG\Asif <asifmehmood.8270@gmail.com>
2024-10-13 10:09:13 +0500

    Hello World

---
+++ hello_world/main/hello_world_main.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+
+/* CONFIG_FREERTOS_HZ 100 - It means that there are 100 ticks per second in the system.
+   1s = 1000ms
+   1s = 100 ticks
+   1000ms = 100 ticks
+   1 tick = 1000 / 100 
+   1 tick = 10ms
+   */
+
+const TickType_t Delay = pdTICKS_TO_MS(7); // 7 * 10 = 70ms
+
+void app_main(void)
+{
+    int counter = 0;
+    while (true)
+    {
+        printf("%d: Hello world!\n\r", counter);
+        counter++;
+        vTaskDelay(Delay);
+    }
+    
+}
